48439f1780219739caea4ce4b8518e9cb971e9c9
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-18 15:26:06 -0800

    adds state, hooks, and components to main also maps over fetched data and passes to country card and select options

--- src/components/Layout/Main/Main.js
+++ src/components/Layout/Main/Main.js
@@ -1,5 +1,38 @@
 import React from 'react';
+import { useAllCountries } from '../../../hooks/useCountries.js';
+import CountryCard from '../../CountryCard/CountryCard.js';
 
 export default function Main() {
-  return <div>Main</div>;
+  const { countries, error } = useAllCountries();
+
+  const filteredCountries = countries.filter((countriesArray) => countriesArray);
+
+  const continents = [...new Set(filteredCountries.map(({ continent }) => continent))];
+
+  const filteredContinents = continents.filter((continent) => {
+    if (continent === null) {
+      return false;
+    } else {
+      return continent;
+    }
+  });
+
+  return (
+    <main>
+      <select className="continent-select">
+        {filteredContinents.map((continent, i) => {
+          return (
+            <option key={i} value={continent}>
+              {continent}
+            </option>
+          );
+        })}
+        ;
+      </select>
+      <p id="error-display">{error}</p>
+      {filteredCountries.map((country) => (
+        <CountryCard key={country.id} {...country} />
+      ))}
+    </main>
+  );
 }
