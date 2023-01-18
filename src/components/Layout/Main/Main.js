febb8d9376f4962783b57cb80126793510470a6d
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-18 15:51:23 -0800

    adds select useState and adds All select option

--- src/components/Layout/Main/Main.js
+++ src/components/Layout/Main/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAllCountries } from '../../../hooks/useCountries.js';
 import CountryCard from '../../CountryCard/CountryCard.js';
 
@@ -9,17 +9,28 @@ export default function Main() {
 
   const continents = [...new Set(filteredCountries.map(({ continent }) => continent))];
 
-  const filteredContinents = continents.filter((continent) => {
+  const filteredContinents = continents.map((continent) => {
     if (continent === null) {
-      return false;
+      continent = 'All';
+      return continent;
     } else {
       return continent;
     }
   });
 
+  const [selectedContinent, setSelectedContinent] = useState('All');
+
+  const handleContinentChange = (e) => {
+    setSelectedContinent(e.target.value);
+  };
+
   return (
     <main>
-      <select className="continent-select">
+      <select
+        className="continent-select"
+        value={selectedContinent}
+        onChange={handleContinentChange}
+      >
         {filteredContinents.map((continent, i) => {
           return (
             <option key={i} value={continent}>
