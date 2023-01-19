4cb1f3906b38dcf0e33dfd99cb4b9ad8c86d6503
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-18 18:00:51 -0800

    adds order and name state as deps to hook

--- src/hooks/useCountries.js
+++ src/hooks/useCountries.js
@@ -2,20 +2,20 @@ import { useEffect, useState } from 'react';
 
 import { fetchAllCountries } from '../services/countries.js';
 
-export function useAllCountries() {
+export function useAllCountries(order, searchByName) {
   const [countries, setCountries] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchCountries = async () => {
       try {
-        const data = await fetchAllCountries();
+        const data = await fetchAllCountries(order, searchByName);
         setCountries(data);
       } catch (e) {
         setError('Error fetching countries, if this persists please contact support.');
       }
     };
     fetchCountries();
-  }, []);
+  }, [order, searchByName]);
   return { countries, error };
 }
