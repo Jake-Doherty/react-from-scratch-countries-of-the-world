f56a0325fe1ca4f01c69eaacb486fcbd0c54db97
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-18 20:07:01 -0800

    adds styles

--- src/components/CountryCard/CountryCard.js
+++ src/components/CountryCard/CountryCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './CountryCard.css';
 
 export default function CountryCard({ name, iso2 }) {
   const iso = iso2.toLowerCase();
@@ -6,7 +7,7 @@ export default function CountryCard({ name, iso2 }) {
     <div className="country-card">
       <div className="details">
         <div className="country-name">{name}</div>
-        <img src={`https://flagcdn.com/16x12/${iso}.png`} alt={name} />
+        <img src={`https://flagcdn.com/${iso}.svg`} alt={name} />
       </div>
     </div>
   );
--- src/components/Layout/Main/Main.js
+++ src/components/Layout/Main/Main.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { useAllCountries } from '../../../hooks/useCountries.js';
 import CountryCard from '../../CountryCard/CountryCard.js';
 
+import background from '../../../background.jpg';
+import './Main.css';
+
 export default function Main() {
   const [order, setOrder] = useState('');
   const [searchByName, setSearchByName] = useState('');
@@ -29,39 +32,58 @@ export default function Main() {
   };
 
   return (
-    <main>
-      <select
-        className="continent-select"
-        value={selectedContinent}
-        onChange={handleContinentChange}
-      >
-        {filteredContinents.map((continent, i) => {
-          return (
-            <option key={i} value={continent}>
-              {continent}
-            </option>
-          );
+    <main
+      style={{
+        backgroundImage: `url(${background})`,
+        // backgroundSize: 'cover',
+        // backgroundRepeat: 'no-repeat',
+        // backgroundPosition: 'center',
+      }}
+    >
+      <div className="search-bar-container">
+        <div className="search-container">
+          <select
+            className="continent-select search-input"
+            value={selectedContinent}
+            onChange={handleContinentChange}
+          >
+            {filteredContinents.map((continent, i) => {
+              return (
+                <option key={i} value={continent}>
+                  {continent}
+                </option>
+              );
+            })}
+            ;
+          </select>
+          <select
+            className="select-order search-input"
+            value={order}
+            onChange={(e) => setOrder(e.target.value)}
+          >
+            <option value="">Select an option</option>
+            <option value="asc">Alphabet Ascending</option>
+            <option value="desc">Alphabet Descending</option>
+          </select>
+          <input
+            className="search-input"
+            placeholder="Search by country name"
+            onChange={(e) => setSearchByName(e.target.value)}
+          />
+        </div>
+        <p id="error-and-load-display">
+          {error} {loadState}
+        </p>
+      </div>
+      <div className="countries-list">
+        {filteredCountries.map((country) => {
+          if (selectedContinent === 'All') {
+            return <CountryCard key={country.id} {...country} />;
+          } else if (country.continent === selectedContinent) {
+            return <CountryCard key={country.id} {...country} />;
+          }
         })}
-        ;
-      </select>
-      <select className="select-order" value={order} onChange={(e) => setOrder(e.target.value)}>
-        <option value="">Select an option</option>
-        <option value="asc">Alphabet Ascending</option>
-        <option value="desc">Alphabet Descending</option>
-      </select>
-      <input
-        placeholder="Search by country name"
-        onChange={(e) => setSearchByName(e.target.value)}
-      />
-      <p id="error-display">{error}</p>
-      <p id="load-state-display">{loadState}</p>
-      {filteredCountries.map((country) => {
-        if (selectedContinent === 'All') {
-          return <CountryCard key={country.id} {...country} />;
-        } else if (country.continent === selectedContinent) {
-          return <CountryCard key={country.id} {...country} />;
-        }
-      })}
+      </div>
       {/* <CountryCard key={country.id} {...country} />} )} */}
     </main>
   );
